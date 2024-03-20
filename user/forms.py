5a10f674ef6fc7f37f83be5c258984467010577e
BuiXuanSon1412 <bxson1412@gmail.com>
2024-03-20 23:27:48 +0700

    Revert "test"
    This reverts commit 66d4ceedda8d13a40f63ae0ab471f5a0b42706f7.

---
+++ user/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2' ]
+
+    def __init__(self, *args, **kwargs):
+        super(MyUserCreationForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
